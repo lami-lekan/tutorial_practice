7e804aece47ad08dbfd4802f4ebd1f92c55aef81
lami_lekan <olagbajuisrael11@gmail.com>
2023-11-03 01:03:14 +0000

    printBoard func

--- c-projects_/tic-tac_game/functions.c
+++ c-projects_/tic-tac_game/functions.c
@@ -19,7 +19,12 @@ void resetBoard(void)
 
 void printBoard(void)
 {
-
+	printf(" %c | %c | %c ", board[0][0], board[0][1], board[0][2]);
+	printf("\n---|---|---\n");
+	printf(" %c | %c | %c ", board[1][0], board[1][1], board[1][2]);
+	printf("\n---|---|---\n");
+	printf(" %c | %c | %c ", board[1][0], board[1][1], board[1][2]);
+	printf("\n");
 }
 
 int checkFreeSpaces(void)
